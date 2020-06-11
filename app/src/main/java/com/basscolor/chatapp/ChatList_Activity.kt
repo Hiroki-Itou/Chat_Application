79ebef6d98cab80e7272f992a2a9900977af7b2e
basscolor-Hiroki <hiroki.piro@gmail.com>
2020-06-11 12:26:29 +0900

    chatlistのUI作成。ListViewへの検証用表示方法を記載

--- app/src/main/java/com/basscolor/chatapp/ChatList_Activity.kt
+++ app/src/main/java/com/basscolor/chatapp/ChatList_Activity.kt
@@ -3,6 +3,7 @@ package com.basscolor.chatapp
 import android.app.Activity
 import android.os.Bundle
 import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 
 class ChatList_Activity : Activity() {
@@ -13,8 +14,23 @@ class ChatList_Activity : Activity() {
 
         val chatList = findViewById<ListView>(R.id.chatList)
 
-        Toast.makeText(baseContext, "Chatへようこそ！！！",
-            Toast.LENGTH_LONG).show()
+        val room_list: MutableList<MutableMap<String,Any>> = mutableListOf()
+        val room = mutableMapOf("imageView" to R.drawable.icon,"name" to "バスカラ", "last_message" to "こんにちは")
+        (1..20).forEach loop@{ _ ->
+            room_list.add(room)
+        }
+
+// SimpleAdapter第4引数from用データの用意
+        val from = arrayOf("imageView","name", "last_message")
+        // SimpleAdapter第5引数to用データの用意
+        val to = intArrayOf(R.id.imageView,R.id.friend_name, R.id.last_message)
+
+        val adapter = SimpleAdapter(this,room_list,R.layout.list_items,from,to)
+
+        chatList.adapter = adapter
+
+//        Toast.makeText(baseContext, "Chatへようこそ！！！",
+//            Toast.LENGTH_LONG).show()
 
     }
 
